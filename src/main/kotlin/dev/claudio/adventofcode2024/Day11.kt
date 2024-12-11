53efac2ae56353eb1e3f24aad34ee47033d18a7d
Claudio <claudio.consolmagno@getmayday.com>
2024-12-11 23:55:18 +0000

    Day 11 Part 2

--- src/main/kotlin/dev/claudio/adventofcode2024/Day11.kt
+++ src/main/kotlin/dev/claudio/adventofcode2024/Day11.kt
@@ -6,45 +6,50 @@ fun main() {
     val day = "day11"
     val sampleList: List<String>? = Support.readFileAsListString("2024/$day-sample.txt")
     val inputList: List<String>? = Support.readFileAsListString("2024/$day-input.txt")
-    val day11part1 = day11part1(inputList!!)
-    if(day11part1 != 189167) throw Error("Incorrect Logic: $day11part1")
-//    println(day11part2(sampleList!!))
-//    println(day11part2(inputList!!))
+    println(day11part1(sampleList!!))
+    println(day11part1(inputList!!))
+    println(day11part2(inputList!!))
 }
 
-fun day11part1(inputList: List<String>): Int {
+fun day11part1(inputList: List<String>): Long {
     return blinkTime(inputList, 25)
 }
 
-fun day11part2(inputList: List<String>): Int {
+fun day11part2(inputList: List<String>): Long {
     return blinkTime(inputList, 75)
 }
 
-private fun blinkTime(inputList: List<String>, blinkCount: Int): Int {
-    var inputAsInt: List<Long> = inputList.flatMap { it.split(" ") }.map { it.toLong() }
-    val memo = mutableMapOf(0L to listOf(1L))
-    repeat(blinkCount) { iteration ->
-        println("Starting blink $iteration with inputList size ${inputAsInt.size}")
-        inputAsInt = inputAsInt.flatMap { runRules(it, memo) }
+private fun blinkTime(inputList: List<String>, blinkCount: Long): Long {
+    val inputAsInt: List<Long> = inputList.flatMap { it.split(" ") }.map { it.toLong() }
+    val memo = mutableMapOf<Pair<Long,Long>, Long>()
+    var count = 1
+    return inputAsInt.sumOf {
+        println("Blinking ${count++}")
+        runRules(it, memo, blinkCount-1)
     }
-    return inputAsInt.count()
 }
 
-fun runRules(stone: Long, memo: MutableMap<Long, List<Long>>): List<Long> {
-    val memoResult = memo[stone]
+fun runRules(stone: Long, memo: MutableMap<Pair<Long,Long>, Long>, count: Long): Long {
+    val memoResult = memo[stone to count]
     if (memoResult != null) {
         return memoResult
     }
     val stoneString = stone.toString()
-    val result = if (stoneString.length % 2 == 0) {
+    val result: List<Long> = if (stoneString.length % 2 == 0) {
         val (left, right) = stoneString.chunked(stoneString.length / 2)
         listOf(
             left.toLong(),
             right.toLong()
         )
+    } else if (stone == 0L){
+        listOf(1L)
     } else {
         listOf(stone * 2024L)
     }
-    memo[stone] = result
-    return result
+    if (count == 0L) {
+        memo[stone to count] = result.count().toLong()
+    } else {
+        memo[stone to count] = result.sumOf { runRules(it, memo, count-1) }
+    }
+    return memo[stone to count]!!
 }
